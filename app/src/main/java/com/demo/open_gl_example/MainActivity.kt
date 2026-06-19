edae23e6d2999bb06809a9ac9fcf11adf5f262fd
PhcBest <phcbest2017@outlook.com>
2026-06-19 23:49:00 +0800

    ✨ feat: 初始化项目并搭建基础 Compose UI 架构

---
+++ app/src/main/java/com/demo/open_gl_example/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.demo.open_gl_example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.demo.open_gl_example.ui.theme.OpenGL_ExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OpenGL_ExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(vertical = 24.dp), horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        item {
+                            Button({
+
+                            }) {
+                                Text("显示一张图片在GL上")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
